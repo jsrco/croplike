6afcd41691fc79c4b2a4ee65fda55f9d6da46d8d
Ryan Orlando <rorlando@evidentchange.org>
2022-11-24 11:43:36 -0800

    Crop-03 storage work setup

---
+++ src/composeables/storage.ts
@@ -0,0 +1,32 @@
+export interface IStorage {
+    getItem(key: string): string | null
+    setItem(key: string, value: string): void
+    removeItem(key: string): void
+  }
+  
+  export abstract class Storage<T extends string> {
+    private readonly storage: IStorage
+  
+    public constructor(getStorage = (): IStorage => window.localStorage) {
+      this.storage = getStorage()
+    }
+  
+    protected get(key: T): string | null {
+      return this.storage.getItem(key)
+    }
+  
+    protected set(key: T, value: string): void {
+      this.storage.setItem(key, value)
+    }
+  
+    protected clearItem(key: T): void {
+      this.storage.removeItem(key)
+    }
+  
+    protected clearItems(keys: T[]): void {
+      for (const key of keys) {
+        this.clearItem(key)
+      }
+    }
+  }
+  
