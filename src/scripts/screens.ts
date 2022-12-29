3b540644defe0aef3815fe170dda5eb624010e2d
Ryan Orlando <rorlando@evidentchange.org>
2022-12-29 14:46:40 -0800

    moved around assests, extra copy fonts for load

--- src/scripts/screens.ts
+++ src/scripts/screens.ts
@@ -3,53 +3,63 @@ import * as PIXI from 'pixi.js'
 
 export const startScreen: PIXI.Application = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight - 36, });
 
-const style = new PIXI.TextStyle({
-    fontFamily: 'PressStart2p',
-    fontSize: 16,
-    fill: ['#4ade80'],
-});
-const richText = new PIXI.Text('a start screen', style);
-richText.x = 10
-richText.y = 10
-startScreen.stage.addChild(richText)
-const poorText = new PIXI.Text('click to go to map demo', style);
-poorText.x = 10
-poorText.y = 30
-startScreen.stage.addChild(poorText)
+const PressStart2P = new FontFace(
+    "PixiPressStart2P",
+    "url('./assets/fonts/PressStart2P-Regular.ttf')"
+);
+PressStart2P.load().then(function (font) {
+    // with canvas, if this is ommited won't work
+    document.fonts.add(font)
+    console.log('font loaded')
 
-startScreen.stage.interactive = true;
-startScreen.stage.hitArea = startScreen.screen;
-startScreen.stage.on('click', (event) => {
-   //handle event
-   useScreen(mapScreen)
-});
+    const style = new PIXI.TextStyle({
+        fontFamily: 'PixiPressStart2P',
+        fontSize: 16,
+        fill: ['#4ade80'],
+    });
+    const richText = new PIXI.Text('a start screen', style);
+    richText.x = 10
+    richText.y = 10
+    startScreen.stage.addChild(richText)
+    const poorText = new PIXI.Text('click to go to map demo', style);
+    poorText.x = 10
+    poorText.y = 30
+    startScreen.stage.addChild(poorText)
 
-const mapScreen: PIXI.Application = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight - 36, });
-const graphics = new PIXI.Graphics();
-graphics.lineStyle(2, 0xFF00FF, 1);
-graphics.beginFill(0x650A5A, 0.25);
-graphics.drawRoundedRect(10, 10, 21, 21, 2);
-graphics.endFill();
-graphics.interactive = true
-graphics.hitArea = new PIXI.RoundedRectangle(10, 10, 21, 21, 2)
-mapScreen.stage.addChild(graphics)
-graphics.on('click', (event) => {
-   //handle event
-    useScreen(startScreen)
-});
-graphics.on('mouseout', (event) => {
-   //handle event
-   graphics.clear()
-   graphics.lineStyle(2, 0xFF00FF, 1);
-graphics.beginFill(0x650A5A, 0.25);
-graphics.drawRoundedRect(10, 10, 21, 21, 2);
-graphics.endFill();
-});
-graphics.on('mouseover', (event) => {
-   //handle event
-   graphics.clear()
-   graphics.lineStyle(2, 0xFF00FF, 1);
-graphics.beginFill(0x650A5A, 0.50);
-graphics.drawRoundedRect(10, 10, 21, 21, 2);
-graphics.endFill();
+    startScreen.stage.interactive = true;
+    startScreen.stage.hitArea = startScreen.screen;
+    startScreen.stage.on('click', (event) => {
+        //handle event
+        useScreen(mapScreen)
+    });
+
+    const mapScreen: PIXI.Application = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight - 36, });
+    const graphics = new PIXI.Graphics();
+    graphics.lineStyle(2, 0xFF00FF, 1);
+    graphics.beginFill(0x650A5A, 0.25);
+    graphics.drawRoundedRect(10, 10, 21, 21, 2);
+    graphics.endFill();
+    graphics.interactive = true
+    graphics.hitArea = new PIXI.RoundedRectangle(10, 10, 21, 21, 2)
+    mapScreen.stage.addChild(graphics)
+    graphics.on('click', (event) => {
+        //handle event
+        useScreen(startScreen)
+    });
+    graphics.on('mouseout', (event) => {
+        //handle event
+        graphics.clear()
+        graphics.lineStyle(2, 0xFF00FF, 1);
+        graphics.beginFill(0x650A5A, 0.25);
+        graphics.drawRoundedRect(10, 10, 21, 21, 2);
+        graphics.endFill();
+    });
+    graphics.on('mouseover', (event) => {
+        //handle event
+        graphics.clear()
+        graphics.lineStyle(2, 0xFF00FF, 1);
+        graphics.beginFill(0x650A5A, 0.50);
+        graphics.drawRoundedRect(10, 10, 21, 21, 2);
+        graphics.endFill();
+    })
 });
