c85ecd30bd412759bedc42c337930a33a8ef82c8
Ryan Orlando <rorlando@evidentchange.org>
2023-01-06 00:55:17 -0800

    replaced graphics with sprites based off textures

--- src/scripts/mapScreen.ts
+++ src/scripts/mapScreen.ts
@@ -1,58 +1,78 @@
-import useScreen from "../composeables/useScreen";
+import useScreen from "../composeables/useScreen"
 import * as PIXI from 'pixi.js'
+import { Container, Graphics, MSAA_QUALITY, Matrix, RenderTexture, Renderer, Sprite } from "pixi.js"
 import { startScreen } from "./startScreen"
-import { ref } from "vue";
 
 const { Screen, ScreenHeight, ScreenWidth } = useScreen()
-export const mapScreen: PIXI.Application = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight - 36, });
+export const mapScreen: PIXI.Application = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight - 36, })
 mapScreen.specialStagename = 'mapScreen'
-const createGridPiece = (pX: number, pY: number, size: number) => {
-    const graphics = new PIXI.Graphics();
-    const curve = 2
-    graphics.lineStyle(2, 0xFEF2F2, 1);
-    graphics.beginFill(0xFCA5A5, 0.25);
-    graphics.drawRoundedRect(pX, pY, size, size, curve);
-    graphics.endFill();
-    graphics.interactive = true
-    graphics.hitArea = new PIXI.RoundedRectangle(pX, pY, size, size, curve)
-    graphics.on('click', (event) => {
-        //handle event
-        Screen.value.stage.removeChildren()
-        useScreen(startScreen)
-    });
-    graphics.on('mouseout', (event) => {
-        //handle event
-        graphics.clear()
-        graphics.lineStyle(2, 0xFEF2F2, 1);
-        graphics.beginFill(0xFCA5A5, 0.25);
-        graphics.drawRoundedRect(pX, pY, size, size, curve);
-        graphics.endFill();
-    });
-    graphics.on('mouseover', (event) => {
-        //handle event
-        graphics.clear()
-        graphics.lineStyle(2, 0xFEF2F2, 1);
-        graphics.beginFill(0xFEF2F2, 0.50);
-        graphics.drawRoundedRect(pX, pY, size, size, curve);
-        graphics.endFill();
-    })
-    mapScreen.stage.addChild(graphics)
-}
+
+const templateShape = new Graphics()
+    .beginFill(0xffffff)
+    .lineStyle({ width: 1, alignment: 0 })
+    .drawRoundedRect(0, 0, 36, 36, 2)
+const { width, height } = templateShape
+
+// Draw the RoundedRect to the RenderTexture
+const renderTexture = RenderTexture.create({
+    width,
+    height,
+    multisample: MSAA_QUALITY.HIGH,
+    resolution: window.devicePixelRatio
+})
+// With the existing renderer, render texture
+// make sure to apply a transform Matrix
+mapScreen.renderer.render(templateShape, {
+    renderTexture,
+    transform: new Matrix(1, 0, 0, 1, 0, 0)
+});
+// Required for MSAA, WebGL 2 only
+(mapScreen.renderer as Renderer).framebuffer.blit()
+// Discard the original Graphics
+templateShape.destroy(true)
 
 export const createGrid = () => {
-    const gridSize = 32
+    mapScreen.stage.removeChildren()
+    const shapes: Sprite[] = []
+    const gridSize = height
     const seperator = gridSize + 3
-    for (let xStarter = 5; xStarter < ScreenWidth.value - seperator - 5; xStarter += seperator) {
-        let yStarter = 5
-        for (yStarter; yStarter < ScreenHeight.value - seperator - 5; yStarter += seperator) {
-            createGridPiece(xStarter, yStarter, gridSize)
+    let counter = 0
+    for (let xStarter = 3; xStarter < ScreenWidth.value - seperator; xStarter += seperator) {
+        let yStarter = 3
+        for (yStarter; yStarter < ScreenHeight.value - seperator; yStarter += seperator) {
+            const shape = new Sprite(renderTexture);
+            shapes[counter] = shape;
+            counter++
+            shape.position.x = xStarter
+            shape.position.y = yStarter
+            shape.interactive = true
+            shape.on('click', (event) => {
+                //handle event
+                mapScreen.stage.removeChildren()
+                useScreen(startScreen)
+            });
+            shape.on('mouseout', (event) => {
+                //handle event
+                shape.tint = 0xffffff
+
+            });
+            shape.on('mouseover', (event) => {
+                //handle event
+                shape.tint = parseInt(Math.floor(Math.random() * 16777215).toString(16), 16);
+            })
         }
     }
+    const container = new Container()
+    container.addChild(...shapes)
+
+    mapScreen.stage.addChild(container)
 }
+
+mapScreen.render()
+requestAnimationFrame(createGrid)
 
 window.addEventListener('resize', () => {
     if (Screen.value.specialStagename === 'mapScreen') {
-        Screen.value.stage.removeChildren()
         createGrid()
     }
 })
--- src/scripts/startScreen.ts
+++ src/scripts/startScreen.ts
@@ -1,13 +1,13 @@
-import useScreen from "../composeables/useScreen";
+import useScreen from "../composeables/useScreen"
 import * as PIXI from 'pixi.js'
-import { createGrid, mapScreen } from "./mapScreen";
+import { createGrid, mapScreen } from "./mapScreen"
 
-export const startScreen: PIXI.Application = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight - 36, });
+export const startScreen: PIXI.Application = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight - 36, })
 
 const PressStart2P = new FontFace(
     "PixiPressStart2P",
     "url('./assets/fonts/PressStart2P-Regular.ttf')"
-);
+)
 PressStart2P.load().then(function (font) {
     // with canvas, if this is ommited won't work
     document.fonts.add(font)
@@ -17,7 +17,7 @@ PressStart2P.load().then(function (font) {
         fontFamily: 'PixiPressStart2P',
         fontSize: 16,
         fill: ['#4ade80'],
-    });
+    })
     const richText = new PIXI.Text('a start screen', style);
     richText.x = 10
     richText.y = 10
@@ -32,6 +32,5 @@ PressStart2P.load().then(function (font) {
     startScreen.stage.on('click', (event) => {
         //handle event
         useScreen(mapScreen)
-        createGrid()
-    });
-});
+    })
+})
