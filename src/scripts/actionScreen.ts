e1ffab64fa6c70d091c34c0aeae33719635d126c
Ryan Orlando <rorlando@evidentchange.org>
2023-02-04 16:37:46 -0800

    Dumb jumping as I teach myself.

--- src/scripts/actionScreen.ts
+++ src/scripts/actionScreen.ts
@@ -17,39 +17,58 @@ const style = new PIXI.TextStyle({
 const square = new PIXI.Graphics()
 const squareDimension = 15
 const vX = ref(0)
+const vY = ref(5)
+const jumping = ref(false)
+const positionChangeY = ref(0)
 const onKeyDown = (key: { keyCode: number }) => {
     if (useScreen().Screen.value?.stageName === 'actionScreen') {
+        if (key.keyCode === 87 || key.keyCode === 38 && positionChangeY.value <= squareDimension) {
+            // If the W key or the Up arrow is pressed, move the player up.
+                jumping.value = true
+                if (vY.value < 0) {
+                    vY.value = 0
+                }
+                if (vY.value < 10) vY.value++
+                positionChangeY.value ++
+                // Don't move to the left if the player is at the left side of the stage
+                square.y -= vY.value
+        }
+
         // A Key is 65
         // Left arrow is 37
         if (key.keyCode === 65 || key.keyCode === 37) {
             // If the A key or the Left arrow is pressed, move the player to the left.
-            if (square.position.x >= 0) {
+            if (square.x >= 0) {
                 if (vX.value > 0) {
                     vX.value = 0
                 }
                 if (vX.value > -5) vX.value--
                 // Don't move to the left if the player is at the left side of the stage
-                square.position.x += vX.value
+                square.x += vX.value
             }
         }
         // D Key is 68
         // Right arrow is 39
         if (key.keyCode === 68 || key.keyCode === 39) {
             // If the D key or the Right arrow is pressed, move the player to the right.
-            if (square.position.x < window.innerWidth - squareDimension) {
+            if (square.x < window.innerWidth - squareDimension) {
                 if (vX.value < 0) {
                     vX.value = 0
                 }
                 if (vX.value < 5) vX.value++
                 // Don't move to the right if the player is at the right side of the stage
-                square.position.x += vX.value
+                square.x += vX.value
             }
         }
     }
 }
+
 const onKeyUp = (key: { keyCode: number }) => {
     if (useScreen().Screen.value?.stageName === 'actionScreen') {
+        jumping.value = false
+        positionChangeY.value = 0
         vX.value = 0
+        vY.value = 5
     }
 }
 document.addEventListener('keydown', onKeyDown)
@@ -83,6 +102,9 @@ export const createAction = () => {
     actionScreen.stage.addChild(square)
 
     const update = () => {
+        if (square.y < window.innerHeight - squareDimension - 36 && !jumping.value) {
+            square.y += 3
+        }
         actionScreen.render()
         requestAnimationFrame(update)
     }
