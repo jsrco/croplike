e75a2ef7e3638283c907facf884191537ca0f63b
Ryan Orlando <rorlando@evidentchange.org>
2023-02-06 22:55:35 -0800

    tweaking numbers

--- src/scripts/entity.ts
+++ src/scripts/entity.ts
@@ -1,54 +1,40 @@
 export class Entity {
     drag: number = 0.95
-    gravity: number = 0.7
+    gravity: number = 0.4
     hanging: boolean = false
-    jumpSpeed: number = 9
-    maxSpeed: number =8
-    minWallSlideSpeed: number = 0.1
+    jumpSpeed: number = 5
+    maxSpeed: number = 3
+    minWallSlideSpeed: number = 0.05
     path: number = 0
     name: string
     size: number = 15
     square: any
     vx: number = 0
     vy: number = 0
-    wallSlideSpeed: number = 2
-    windowHeightDummy: number
+    wallSlideSpeed: number = 1
+    wallJumpSpeed: number = 5
+    windowHeightDummy: number = window.innerHeight - 36 - this.size
     constructor(square: any, name: string) {
         this.name = name
         this.square = square
-        this.windowHeightDummy = window.innerHeight - 36 - this.size
     }
-    private checkForCollisions(entities: Entity[]) {
+    detectCollision(entities: Entity[]) {
         for (const entity of entities) {
-            if (entity === this) continue
-            if (this.isCollidingWith(entity)) {
-                if (this.name === 'player') {
-                    let angle = Math.atan2(this.square.y - entity.square.y, this.square.x - entity.square.x)
-                    let xDistance = Math.cos(angle) * 0.5
-                    let yDistance = Math.sin(angle) * 0.5
-                    let overlap = this.size + entity.size - this.getDistance(this, entity)
-                    entity.square.x += xDistance * overlap
-                    entity.square.y += yDistance * overlap
-                }
+            if (entity === this) continue;
+            if (this.square.x + this.size >= entity.square.x && this.square.x <= entity.square.x + entity.size
+                && this.square.y + this.size >= entity.square.y && this.square.y <= entity.square.y + entity.size) {
+                console.log(`Collision detected between ${this.name} and ${entity.name}`)
+                break
             }
         }
-    }
-    private getDistance(entity1: Entity, entity2: Entity) {
-        let a = entity1.square.x - entity2.square.x
-        let b = entity1.square.y - entity2.square.y
-        return Math.sqrt(a * a + b * b)
-    }
-    private isCollidingWith(other: Entity) {
-        const x1 = this.square.x
-        const y1 = this.square.y
-        const x2 = other.square.x
-        const y2 = other.square.y
-        return x1 < x2 + other.size && x1 + this.size > x2 &&
-            y1 < y2 + other.size && y1 + this.size > y2
+        if (this.square.x <= 0 || this.square.x + this.size >= window.innerWidth ||
+            this.square.y < 0 || this.square.y + this.size > window.innerHeight) {
+            console.log('Collision detected with the edge of the map')
+        }
     }
     moveLeft() {
         if (!this.hanging) {
-            this.vx -= 5
+            this.vx -= this.maxSpeed;
             this.vx = Math.max(this.vx, -this.maxSpeed)
         } else if (this.vx > 0) {
             this.vx = -this.wallSlideSpeed
@@ -56,7 +42,7 @@ export class Entity {
     }
     moveRight() {
         if (!this.hanging) {
-            this.vx += 5
+            this.vx += this.maxSpeed
             this.vx = Math.min(this.vx, this.maxSpeed)
         } else if (this.vx < 0) {
             this.vx = this.wallSlideSpeed
@@ -70,7 +56,7 @@ export class Entity {
     }
     wallJump() {
         if (this.hanging) {
-            this.vy = -this.jumpSpeed
+            this.vy = -this.wallJumpSpeed
             this.vx = this.vx * -1.5
             this.hanging = false
         }
@@ -79,13 +65,15 @@ export class Entity {
         this.square.x += this.vx
         this.square.y += this.vy
         this.vy += this.gravity
+
         if (this.square.y > this.windowHeightDummy - 1) {
             this.square.y = this.windowHeightDummy
             this.vy = 0
             this.hanging = false
         }
+
         if (this.square.x < 0) {
-            this.square.x = 0
+            this.square.x = 0;
             if (this.vx < 0) {
                 this.vy = this.wallSlideSpeed
                 this.updateWallSlideSpeed()
@@ -107,12 +95,12 @@ export class Entity {
             this.hanging = false
         }
         this.vx *= this.drag
-        this.checkForCollisions(entities)
+        this.detectCollision(entities)
     }
     private updateWallSlideSpeed() {
         this.wallSlideSpeed *= 0.9
         if (this.wallSlideSpeed < this.minWallSlideSpeed) {
-            this.wallSlideSpeed = 0
+            this.wallSlideSpeed = this.minWallSlideSpeed
         }
     }
 }
