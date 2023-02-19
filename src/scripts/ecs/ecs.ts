def333a09765ca818e6f3ff74bb5109a494f160c
Ryan Orlando <rorlando@evidentchange.org>
2023-02-18 23:48:44 -0800

    small correction

--- src/scripts/ecs/ecs.ts
+++ src/scripts/ecs/ecs.ts
@@ -31,14 +31,12 @@ export class ECS {
     }
     public addEntity(entity: Entity): void {
         this.entityManager.addEntity(entity)
-        this.app.stage.addChild(entity)
     }
     public addSystem(system: System): void {
         this.entityManager.addSystem(system)
     }
     public removeEntity(entity: Entity): void {
         this.entityManager.removeEntity(entity)
-        this.app.stage.removeChild(entity)
     }
     public removeSystem(system: System): void {
         this.entityManager.removeSystem(system)
