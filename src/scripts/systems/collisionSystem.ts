f5b1732d6d69879534933ef27786557e5db66268
Ryan Orlando <rorlando@evidentchange.org>
2023-02-25 15:46:25 -0800

    reset

--- src/scripts/systems/collisionSystem.ts
+++ src/scripts/systems/collisionSystem.ts
@@ -33,30 +33,10 @@ export class CollisionSystem extends System {
         return collisionA.rectangle.intersects(collisionB.rectangle)
     }
     private getCollisionSide(entityA: Entity, entityB: Entity): string {
-        const positionA = entityA.getComponent('position') as PositionComponent
-        const positionB = entityB.getComponent('position') as PositionComponent
-
-        const sizeA = entityA.getComponent('size') as SizeComponent
-        const sizeB = entityB.getComponent('size') as SizeComponent
-
-        const xDiff = positionA.x - positionA.previousPositionX
-        const yDiff = positionA.y - positionA.previousPositonY
-        const otherLeft = positionB.x
-        const otherRight = positionB.x + sizeB.width
-        const otherTop = positionB.y
-        const otherBottom = positionB.y + sizeB.height
-
-        if (yDiff > 0 && positionA.y < otherTop && positionA.previousPositonY >= otherTop) {
-            return 'top'
-        } else if (yDiff < 0 && positionA.y + sizeA.height > otherBottom && positionA.previousPositonY + sizeA.height <= otherBottom) {
-            return 'bottom'
-        } else if (xDiff > 0 && positionA.x < otherLeft && positionA.previousPositionX >= otherLeft) {
-            return 'left'
-        } else if (xDiff < 0 && positionA.x + sizeA.width > otherRight && positionA.previousPositionX + sizeA.width <= otherRight) {
-            return 'right'
-        }
-        return ''
+        // getCollisionSide 
+        return 'side the entity is touching'
     }
+
     private handleCollision(entityA: Entity, entityB: Entity): void {
         // Implement collision response here
         const velocityA = entityA.getComponent('velocity') as VelocityComponent
