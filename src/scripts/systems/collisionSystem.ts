c07fbd8dfd1340ef9b115ea1da99e80edab12b34
Ryan Orlando <jsrco@hotmail.com>
2023-02-25 17:43:41 -0800

    update logic

--- src/scripts/systems/collisionSystem.ts
+++ src/scripts/systems/collisionSystem.ts
@@ -10,37 +10,42 @@ export class CollisionSystem extends System {
 
     update(deltaTime: number): void {
         const entities = this.world.getEntitiesByComponent('collision', 'position', 'velocity')
-        // check for collisions and the side they occured on
-        // handle collision logic
+        for (let i = 0; i < entities.length; i++) {
+            const entityA = entities[i]
+            for (let j = i + 1; j < entities.length; j++) {
+                const entityB = entities[j]
+                const collisionB = entityB.getComponent('collision') as CollisionComponent
+                const check = this.checkCollision(entityA, entityB)
+                if (check[0]) {
+                    this.handleCollision(entityA, entityB, check[1])
+                }
+            }
+        }
+
     }
+    private checkCollision(entityA: Entity, entityB: Entity): [boolean, string] {
+        const collisionA = entityA.getComponent('collision') as CollisionComponent
+        const collisionB = entityB.getComponent('collision') as CollisionComponent
 
-    private checkCollision(
-        collisionA: CollisionComponent,
-        collisionB: CollisionComponent
-    ): boolean {
-        return collisionA.rectangle.intersects(collisionB.rectangle)
+        const itDidHappen = collisionA.rectangle.intersects(collisionB.rectangle)
+        const collisionSide: string = 'bottom' || 'left' || 'right' || 'top'
+
+        if (itDidHappen) {
+            const positionA = entityA.getComponent('position') as PositionComponent
+            const positionB = entityB.getComponent('position') as PositionComponent
+            const sizeA = entityA.getComponent('size') as SizeComponent
+            const sizeB = entityB.getComponent('size') as SizeComponent
+            // implment logic to check which side the collision occured on
+        }
+
+        return [itDidHappen, collisionSide]
     }
-    private getCollisionSide(entityA: Entity, entityB: Entity): string {
-        const positionA = entityA.getComponent('position') as PositionComponent
-        const positionB = entityB.getComponent('position') as PositionComponent
-
-        const sizeA = entityA.getComponent('size') as SizeComponent
-        const sizeB = entityB.getComponent('size') as SizeComponent
-
-        const collisionSide = false || 'top' || 'right' || 'down' || 'left'
-
-        // logic for determening collisionSide goes here
-
-        return collisionSide
-    }
-
-    private handleCollision(entityA: Entity, entityB: Entity): void {
+    private handleCollision(entityA: Entity, entityB: Entity, whereItHappened: string): void {
         const velocityA = entityA.getComponent('velocity') as VelocityComponent
         const velocityB = entityB.getComponent('velocity') as VelocityComponent
 
         // Implement collision response here
-        // check Collisions have been called determine where the entities have collied by getCollisionSide
-        // handle the collision basesd of the entities side
+        // handle the collision basesd off whereItHappened
         // collisions on left and right, should set the entity x veloicty to 0 to prevent movement left and right 
         // collisions on the top and bottom, should set the entity y veloicty to 0 to prevent gravity and jumping. 
 
