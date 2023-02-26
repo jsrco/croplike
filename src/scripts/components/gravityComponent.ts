454611803777491b779e37820bb683069b7fdcb2
Ryan Orlando <jsrco@hotmail.com>
2023-02-25 18:28:36 -0800

    collision system start implmented

--- src/scripts/components/gravityComponent.ts
+++ src/scripts/components/gravityComponent.ts
@@ -8,6 +8,12 @@ export class GravityComponent extends Component {
     
     constructor(world: World) {
         super(world)
+        this.world.eventManager.subscribe('positionChange', this.onPositionChange.bind(this))
+    }
+    private onPositionChange(data: any): void {
+        if (data.entity === this.owner) {
+            this.setGroundStatus(false)
+        }
     }
     setGravity(x: number) {
         this.force = x
