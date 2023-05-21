140f4b6c45a0f050413e1f2e8bc6afb86d21c706
Ryan Orlando <rorlando@evidentchange.org>
2023-05-21 12:47:11 -0700

    Various mobile corrections to controls and style. F IOS

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <Navigation />
   <Screen />
-  <!-- <Controls /> -->
+  <Controls />
   <Info />
 </template>
 
 <script setup lang="ts">
-// import Controls from "./components/Controls.vue"
+import Controls from "./components/Controls.vue"
 import Info from "./components/Info.vue"
 import Navigation from "./components/Navigation.vue"
 import Screen from "./components/Screen.vue"
--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,38 +1,51 @@
 <template>
-    <div class="flex z-10 absolute bottom-0 left-0 controls w-screen">
-      <!-- Left div -->
-      <div class="w-1/2" @pointerdown="goLeft()" @pointerup="game.resetMovement()"></div>
-      <!-- Right div -->
-      <div class="w-1/2" @pointerdown="goRight()" @pointerup="game.resetMovement()"></div>
-    </div>
-    <div v-if="game.paused.value !== true" class="z-20 absolute bottom-4 right-4 font-share text-yellow-400" @pointerdown="goJump()" @pointerup="game.resetMovement()">
-      jump
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48" stroke-width="1.5" stroke="currentColor"
+  <div v-if="game.paused.value !== true && smolScreen()"
+       class="absolute bottom-4 left-4 flex flex-row gap-x-2">
+    <div class="border border-3 rounded-lg border-yellow-400 z-20 font-share text-yellow-400" @pointerdown="goLeft()"
+      @pointerup="game.resetMovement()">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
         class="w-12 h-12">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
       </svg>
     </div>
-  </template>
+    <div class="border border-3 rounded-lg border-yellow-400 z-20 font-share text-yellow-400" @pointerdown="goRight()"
+      @pointerup="game.resetMovement()">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="w-12 h-12">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+      </svg>
+    </div>
+  </div>
+
+  <div v-if="game.paused.value !== true && smolScreen" class="border border-3 rounded-lg border-yellow-400 z-20 absolute bottom-4 right-4 font-share text-yellow-400"
+    @pointerdown="goJump()" @pointerup="game.resetMovement()">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+      class="w-12 h-12">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+    </svg>
+  </div>
+</template>
   
-  <script setup lang="ts">
-  import useEngine from "../composeables/use-engine"
+<script setup lang="ts">
+import useEngine from "../composeables/use-engine"
+import { smolScreen } from "../scripts/util/Tools"
+
+const { game } = useEngine()
+
+const goJump = () => {
+  if (game.paused.value !== true) game.playerMoveJump()
+}
+const goLeft = () => {
+  if (game.paused.value !== true) game.playerMoveLeft()
+}
+const goRight = () => {
+  if (game.paused.value !== true) game.playerMoveRight()
+}
+
+</script>
   
-  const { game } = useEngine()
-  
-  const goJump = () => {
-    if (game.paused.value !== true) game.playerMoveJump()
-  }
-  const goLeft = () => {
-    if (game.paused.value !== true) game.playerMoveLeft()
-  }
-  const goRight = () => {
-    if (game.paused.value !== true) game.playerMoveRight()
-  }
-  
-  </script>
-  
-  <style>
-  .controls {
-    height: calc(100vh - 36px);
-  }
-  </style>{}
+<style>
+.controls {
+  height: calc(100vh - 36px);
+}
+</style>{}
