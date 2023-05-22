ac3edf83696e57a50af99ab3ef32a6f38bf321f5
Ryan Orlando <rorlando@evidentchange.org>
2023-05-21 23:33:39 -0700

    removed stuff not used

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <Navigation />
   <Screen />
-  <!-- <Controls /> -->
   <Info />
 </template>
 
 <script setup lang="ts">
-// import Controls from "./components/Controls.vue"
 import Info from "./components/Info.vue"
 import Navigation from "./components/Navigation.vue"
 import Screen from "./components/Screen.vue"
