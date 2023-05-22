417c41658cbe659dbfedb61e53ea66e2b0080d9f
Ryan Orlando <rorlando@evidentchange.org>
2023-05-21 20:23:04 -0700

    more expriments.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,15 +1,15 @@
 <template>
   <div v-if="game.paused.value !== true && smolScreen()"
        class="absolute bottom-4 left-4 flex flex-row gap-x-2">
-    <div class="border border-3 rounded-lg border-yellow-400 z-20 font-share text-yellow-400" @pointerdown="goLeft()"
-      @pointerup="game.resetMovement()">
+    <div class="border border-3 rounded-lg border-yellow-400 z-20 font-share text-yellow-400" @touchstart="goLeft()"
+      @touchmove="game.resetMovement()">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
         class="w-12 h-12">
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
       </svg>
     </div>
-    <div class="border border-3 rounded-lg border-yellow-400 z-20 font-share text-yellow-400" @pointerdown="goRight()"
-      @pointerup="game.resetMovement()">
+    <div class="border border-3 rounded-lg border-yellow-400 z-20 font-share text-yellow-400" @touchstart="goRight()"
+      @touchmove="game.resetMovement()">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
         class="w-12 h-12">
         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
@@ -18,7 +18,7 @@
   </div>
 
   <div v-if="game.paused.value !== true && smolScreen()" class="border border-3 rounded-lg border-yellow-400 z-20 absolute bottom-4 right-4 font-share text-yellow-400"
-    @pointerdown="goJump()" @pointerup="game.resetMovement()">
+    @touchstart="goJump()" @touchmove="game.resetMovement()">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
       class="w-12 h-12">
       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
