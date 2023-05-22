3834fd3c9b8fe9b59d84401c320427ffe37ac87b
Ryan Orlando <rorlando@evidentchange.org>
2023-05-21 20:38:38 -0700

    last check before I wipe mobile support

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,14 +1,14 @@
 <template>
   <div v-if="game.paused.value !== true && smolScreen()"
-       class="absolute bottom-4 left-4 flex flex-row gap-x-2 touch-none">
-    <div class="border border-3 rounded-lg border-yellow-400 z-20 font-share text-yellow-400" @touchstart="goLeft()"
+       class="absolute bottom-4 left-4 flex flex-row gap-x-2">
+    <div class="border border-3 rounded-lg border-yellow-400 z-20 text-yellow-400" @touchstart="goLeft()"
       @touchmove="game.resetMovement()">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
         class="w-12 h-12">
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
       </svg>
     </div>
-    <div class="border border-3 rounded-lg border-yellow-400 z-20 font-share text-yellow-400" @touchstart="goRight()"
+    <div class="border border-3 rounded-lg border-yellow-400 z-20 text-yellow-400" @touchstart="goRight()"
       @touchmove="game.resetMovement()">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
         class="w-12 h-12">
@@ -17,7 +17,7 @@
     </div>
   </div>
 
-  <div v-if="game.paused.value !== true && smolScreen()" class="border border-3 rounded-lg border-yellow-400 z-20 absolute bottom-4 right-4 font-share text-yellow-400"
+  <div v-if="game.paused.value !== true && smolScreen()" class="border border-3 rounded-lg border-yellow-400 z-20 absolute bottom-4 right-4 text-yellow-400"
     @touchstart="goJump()" @touchmove="game.resetMovement()">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
       class="w-12 h-12">
