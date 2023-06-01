1cfec474ed5cd3622d99f5909b4c6603f9d2eb1b
Ryan Orlando <rorlando@evidentchange.org>
2023-06-01 00:02:08 -0700

    remove wall extra

--- src/scripts/components/wallComponent.ts
+++ src/scripts/components/wallComponent.ts
@@ -3,7 +3,6 @@ import { Component } from "."
 import { World } from "../util/World"
 
 export class WallComponent extends Component {
-    isMobile: boolean = false
     isWall: boolean = true
     type: string = 'wall'
 
