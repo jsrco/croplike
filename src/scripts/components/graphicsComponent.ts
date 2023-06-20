4a77f9d1b5a04b2d9e485b6ce0ce64b834e169d4
Ryan Orlando <rorlando@evidentchange.org>
2023-06-19 20:59:47 -0700

    replaced graphic with sprite

--- src/scripts/components/graphicsComponent.ts
+++ src/scripts/components/graphicsComponent.ts
@@ -6,35 +6,35 @@ import { World } from "../util/World"
 
 export class GraphicsComponent extends Component {
     color: any
-    rectangle: Graphics
+    sprite: Sprite
     type: string = 'graphics'
 
     constructor(entity: Entity, world: World, color = 0x00000) {
         super(entity, world)
         this.color = color
-        this.rectangle = new PIXI.Graphics().beginFill(this.color).drawRect(0, 0, 10, 10)
-
+        this.sprite = new PIXI.Sprite(PIXI.Texture.WHITE)
+        this.sprite.tint = color
         this.world.eventManager.subscribe('positionChange', this.onPositionChange.bind(this))
         this.world.eventManager.subscribe('sizeChange', this.onSizeChange.bind(this))
     }
     addToStage() {
-        this.world.app.stage.addChild(this.rectangle)
+        this.world.app.stage.addChild(this.sprite)
     }
     onPositionChange(data: any): void {
         if (data.entity === this.owner) {
             const positionComponent = data.positionComponent
-            this.rectangle.x = positionComponent.x
-            this.rectangle.y = positionComponent.y
+            this.sprite.x = positionComponent.x
+            this.sprite.y = positionComponent.y
         }
     }
     onSizeChange(data: any): void {
         if (data.entity === this.owner) {
             const sizeComponent = data.sizeComponent
-            this.rectangle.height = sizeComponent.height
-            this.rectangle.width = sizeComponent.width
+            this.sprite.height = sizeComponent.height
+            this.sprite.width = sizeComponent.width
         }
     }
     removeFromStage() {
-        this.world.app.stage.removeChild(this.rectangle)
+        this.world.app.stage.removeChild(this.sprite)
     }
 }
