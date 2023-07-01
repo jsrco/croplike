0447a794253cc64c47793317ad5d1c0214fb93be
Ryan Orlando <rorlando@evidentchange.org>
2023-07-01 13:16:20 -0700

    Lesson 17

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Lesson16/>
+  <Lesson17/>
   <!--
   <Navigation />
   <Screen />
@@ -9,18 +9,7 @@
 
 <script setup lang="ts">
 import Info from "./components/Info.vue"
-import Lesson03 from "./components/lessons/Lesson03.vue"
-import Lesson05 from "./components/lessons/Lesson05.vue"
-import Lesson06 from "./components/lessons/Lesson06.vue"
-import Lesson07 from "./components/lessons/Lesson07.vue"
-import Lesson08 from "./components/lessons/Lesson08.vue"
-import Lesson09 from "./components/lessons/Lesson09.vue"
-import Lesson10 from "./components/lessons/Lesson10.vue"
-import Lesson11 from "./components/lessons/Lesson11.vue"
-import Lesson12 from "./components/lessons/Lesson12.vue"
-import Lesson13 from "./components/lessons/Lesson13.vue"
-import Lesson15 from "./components/lessons/Lesson15.vue"
-import Lesson16 from "./components/lessons/Lesson16.vue"
+import Lesson17 from "./components/lessons/Lesson17.vue"
 import Navigation from "./components/Navigation.vue"
 import Screen from "./components/Screen.vue"
 </script>
