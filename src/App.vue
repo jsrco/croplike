c9d6ceac2e49f2b9f35684f1f996ef11e9d59f72
Ryan Orlando <rorlando@evidentchange.org>
2023-07-01 16:14:00 -0700

    Lesson 19

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Lesson17/>
+  <Lesson19/>
   <!--
   <Navigation />
   <Screen />
@@ -9,7 +9,7 @@
 
 <script setup lang="ts">
 import Info from "./components/Info.vue"
-import Lesson17 from "./components/lessons/Lesson17.vue"
+import Lesson19 from "./components/lessons/Lesson19.vue"
 import Navigation from "./components/Navigation.vue"
 import Screen from "./components/Screen.vue"
 </script>
