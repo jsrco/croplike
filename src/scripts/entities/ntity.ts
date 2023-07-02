0549de64746e887435f55d0a18958c6b8f5d80be
Ryan Orlando <rorlando@evidentchange.org>
2023-07-02 13:19:40 -0700

    typo correct

---
+++ src/scripts/entities/ntity.ts
@@ -0,0 +1,32 @@
+import { Component } from "../components"
+
+export class Entity {
+  components: Record<string, Component>
+  id: number
+  name: string
+
+  constructor(name: string) {
+    this.components = {}
+    this.id = new Date().getTime()
+    this.name = name
+  }
+  addComponent(component: Component): void {
+    this.components[component.type] = component
+    component.owner = this
+  }
+  addComponents(components: Component[]): void {
+    components.forEach((component) => {
+      this.addComponent(component)
+      component.owner = this
+    })
+  }
+  getComponent(componentType: string): Component | undefined {
+    return this.components[componentType]
+  }
+  hasComponent(componentType: string): boolean {
+    return this.components.hasOwnProperty(componentType)
+  }
+  removeComponent(componentType: string): void {
+    delete this.components[componentType]
+  }
+}
