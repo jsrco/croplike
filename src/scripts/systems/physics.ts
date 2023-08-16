9679b6cae9bcb5cc761a259508ed1b89c47a2280
Ryan Orlando <rorlando@evidentchange.org>
2023-08-16 15:32:40 -0700

    collision update

--- src/scripts/systems/physics.ts
+++ src/scripts/systems/physics.ts
@@ -4,6 +4,12 @@ import { RapierComponent } from "../components/rapier"
 import { System } from "./system"
 import { World } from "../util/world"
 
+export type Collision = {
+  bottom: boolean
+  left: boolean
+  right: boolean
+  top: boolean
+}
 export class PhysicsSystem extends System {
 
   showColliderBounds: boolean = true
@@ -17,94 +23,111 @@ export class PhysicsSystem extends System {
 
   update(deltaTime: number): void {
     this.world.physicsWorld.step(this.world.physicsWorldEventQueue)
-    this.world.physicsWorldEventQueue.drainCollisionEvents((handle1, handle2, started) => {
-      /* Handle the collision event. */
-      const entity1 = this.world.getEntityByHandle(handle1)
-      const entity2 = this.world.getEntityByHandle(handle2)
-      
-      if (!started) {
-        const rapierComponent1 = entity1?.getComponent('rapier') as RapierComponent
-        const rapierComponent2 = entity2?.getComponent('rapier') as RapierComponent
-        if (rapierComponent1) rapierComponent1.setIsColliding(false)
-        if (rapierComponent2) rapierComponent2.setIsColliding(false)
-      }
-
-      this.world.physicsWorld.narrowPhase.contactPair(handle1, handle2, (manifold, flipped) => {
-        // Contact information can be read from `manifold`.
-        if (entity1 && entity2) {
-          const normal1 = manifold.localNormal1()
-          const normal2 = manifold.localNormal2()
-          // Check if collision occurred on the bottom of either entity
-          const bottomCollision1 = normal1.y > 0 || normal1.y > normal2.y
-          const bottomCollision2 = normal2.y > 0 || normal2.y > normal1.y
-          const rapierComponent1 = entity1.getComponent('rapier') as RapierComponent
-          const rapierComponent2 = entity2.getComponent('rapier') as RapierComponent
-          if (bottomCollision1) {
-            rapierComponent1.setIsOnGround(true)
-            if (entity2.name !== 'wall') rapierComponent1.setVelocity({ x: rapierComponent2.velocity.x === 0 ? rapierComponent1.velocity.x : rapierComponent2.velocity.x, y: rapierComponent1.velocity.y,})
-            rapierComponent2.setIsStoodOn(true)
-          } else if (bottomCollision2) {
-            rapierComponent2.setIsOnGround(true)
-            if (entity1.name !== 'wall') rapierComponent2.setVelocity({ x: rapierComponent1.velocity.x === 0 ? rapierComponent2.velocity.x : rapierComponent1.velocity.x, y: rapierComponent2.velocity.y,})
-            rapierComponent1.setIsStoodOn(true)
-          } else if (!bottomCollision1 && !bottomCollision2) {
-            rapierComponent1.setIsOnGround(false)
-            rapierComponent1.setIsStoodOn(false)
-            rapierComponent2.setIsOnGround(false)
-            rapierComponent2.setIsStoodOn(false)
-          }
-        }
-      })
-    })
 
     const entities = this.getEntitiesByComponent('rapier', 'pixi')
     for (const entity of entities) {
       const pixiComponent = entity.getComponent('pixi') as PixiComponent
       const rapierComponent = entity.getComponent('rapier') as RapierComponent
       const colliderInfo = rapierComponent.collider.shape as RAPIER.Cuboid
-      
+      const CollisionStatus: Collision = {
+        bottom: false,
+        left: false,
+        right: false,
+        top: false,
+      }
       // Check if unit is in contact
       const contacts = []
       this.world.physicsWorld.contactsWith(rapierComponent.collider, (otherCollider) => {
         contacts.push(otherCollider)
 
-        // this.world.physicsWorld.contactPair(rapierComponent.collider, otherCollider,  (manifold, flipped) => {
-        //   console.log('detect collision type and then correct status', manifold.localNormal1())
-        // })
+        this.world.physicsWorld.contactPair(rapierComponent.collider, otherCollider, (manifold, flipped) => {
+          const localNormal = flipped ? manifold.localNormal2() : manifold.localNormal1()
+
+          // Define the world up vector for a 2D game
+          const upVector = { x: 0, y: 1 }
+
+          // Calculate the cross product (z component) between the contact normal and the world up vector
+          const crossProductZ = localNormal.x * upVector.y - localNormal.y * upVector.x
+
+          // Define thresholds for identifying collision sides
+          const verticalThreshold = 0.8 // Adjust as needed
+          const horizontalThreshold = 0.8 // Adjust as needed
+
+          // Determine which side of the collider the collision occurred on
+          if (Math.abs(crossProductZ) < verticalThreshold) {
+            // Vertical collision
+            if (localNormal.y > 0) {
+              CollisionStatus.bottom = true
+            } else if (localNormal.y < 0) {
+              CollisionStatus.top = true
+            }
+          } else if (Math.abs(crossProductZ) < horizontalThreshold) {
+            // Horizontal collision
+            if (localNormal.x > 0) {
+              CollisionStatus.right = true
+            } else if (localNormal.x < 0) {
+              CollisionStatus.left = true
+            }
+          }
+        })
       })
       if (contacts.length === 0) {
         rapierComponent.setIsColliding(false)
         rapierComponent.setIsOnGround(false)
         rapierComponent.setIsStoodOn(false)
       }
-      if (contacts.length > 0) rapierComponent.setIsColliding(true)
-      if (contacts.length > 0 && rapierComponent.canGrow) {
-        if(colliderInfo.halfExtents.y > 10 && rapierComponent.isStoodOn) {
-          colliderInfo.halfExtents.y -= .5
-          rapierComponent.collider.setHalfExtents(colliderInfo.halfExtents)
-          const newPixiSize = { x: colliderInfo.halfExtents.x * 2, y: colliderInfo.halfExtents.y * 2}
-          pixiComponent.setSize(newPixiSize)
-          const position = rapierComponent.body.translation()
-          position.y += 1
-          rapierComponent.body.setTranslation(position,true)
+      if (contacts.length > 0) {
+        rapierComponent.setIsColliding(true)
+        if (CollisionStatus.bottom) rapierComponent.setIsOnGround(true)
+        else rapierComponent.setIsOnGround(false)
+        if (CollisionStatus.top) rapierComponent.setIsStoodOn(true)
+        else rapierComponent.setIsStoodOn(false)
+      }
+      if (rapierComponent.canGrow) {
+        if (colliderInfo.halfExtents.y > 10 && CollisionStatus.top) {
+          const newSizeY = colliderInfo.halfExtents.y - 0.2
+          if (newSizeY >= 10) {
+            // Reduce size and update physics properties gradually
+            const newSize = { ...colliderInfo.halfExtents }
+            newSize.y = newSizeY
+            colliderInfo.halfExtents = newSize
+            rapierComponent.collider.setHalfExtents(newSize)
+            const newPixiSize = { x: newSize.x * 2, y: newSize.y * 2 }
+            pixiComponent.setSize(newPixiSize)
+
+            // Gradually update position
+            const position = rapierComponent.body.translation()
+            position.y += 0.1 // Adjust the step size as needed
+            rapierComponent.body.setTranslation(position, true)
+          }
         }
 
-        if(colliderInfo.halfExtents.y < pixiComponent.maxSize / 2 && !rapierComponent.isStoodOn && rapierComponent.canGrow) {
-          colliderInfo.halfExtents.y += .5
-          rapierComponent.collider.setHalfExtents(colliderInfo.halfExtents)
-          const newPixiSize = { x: colliderInfo.halfExtents.x * 2, y: colliderInfo.halfExtents.y * 2}
-          pixiComponent.setSize(newPixiSize)
-          const position = rapierComponent.body.translation()
-          position.y -= 1
-          rapierComponent.body.setTranslation(position,true)
+        if (colliderInfo.halfExtents.y < pixiComponent.maxSize / 2 && !CollisionStatus.top) {
+          const newSizeY = colliderInfo.halfExtents.y + 0.2
+          if (newSizeY <= pixiComponent.maxSize / 2) {
+            // Increase size and update physics properties gradually
+            const newSize = { ...colliderInfo.halfExtents }
+            newSize.y = newSizeY
+            colliderInfo.halfExtents = newSize
+            rapierComponent.collider.setHalfExtents(newSize)
+            const newPixiSize = { x: newSize.x * 2, y: newSize.y * 2 }
+            pixiComponent.setSize(newPixiSize)
+
+            // Gradually update position
+            const position = rapierComponent.body.translation()
+            position.y -= 0.1 // Adjust the step size as needed
+            rapierComponent.body.setTranslation(position, true)
+          }
         }
+
+        // Make sure to handle other collision status updates here
       }
 
       if (this.showColliderBounds) rapierComponent.updateGraphics()
       else if (!rapierComponent.cleared) rapierComponent.clearColliderGraphics()
       const position = rapierComponent.body.translation()
       // Pixi renders the sprites starting from the center of the collider
-      position.x -= colliderInfo.halfExtents.x 
+      position.x -= colliderInfo.halfExtents.x
       position.y -= colliderInfo.halfExtents.y
       pixiComponent.setPosition(position)
     }
