d177a293867ce5125b0b732ee66edd4315a00f9f
Ryan Orlando <rorlando@evidentchange.org>
2023-12-29 23:46:14 -0800

    layout adjust for About and Game pages

--- src/components/landing/Content.vue
+++ src/components/landing/Content.vue
@@ -1,20 +1,28 @@
 <template>
-    <div class="py-24 sm:py-32">
-      <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-          <h1 class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl font-share">A better workflow</h1>
-          <div class="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-white lg:max-w-none lg:grid-cols-2 font-roboto">
-            <div>
-              <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
-              <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas.</p>
-            </div>
-            <div>
-              <p>Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim eget. Est augue maecenas risus nulla ultrices congue nunc tortor.</p>
-              <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
-            </div>
+  <div class="py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+        <h1 class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl font-share">A better workflow</h1>
+        <div
+          class="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 lg:max-w-none lg:grid-cols-2 font-roboto">
+          <div>
+            <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet
+              vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat
+              velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed
+              amet vitae sed turpis id.</p>
+            <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit
+              molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et
+              ultrices hac adipiscing egestas.</p>
           </div>
+          <div>
+            <p>Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim eget.
+              Est augue maecenas risus nulla ultrices congue nunc tortor.</p>
+            <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit
+              molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et
+            ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</div></template>
   
--- src/components/pages/Game.vue
+++ src/components/pages/Game.vue
@@ -1,9 +1,18 @@
 <template>
-    <div class="text text-white font-share bg-dirt">
-        Game vue.js
+    <div :style="{ width: `${stageX}px`, height: `${stageY}px` }"
+        class="absolute inset-0 text-white z-50 sm:px-6 lg:px-8 p-4">
+        <!-- Content goes here -->
     </div>
 </template>
   
+  
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const stageX = ref(self.innerWidth)
+const stageY = ref(self.innerHeight)
+window.addEventListener('resize', () => {
+    stageX.value = self.innerWidth
+    stageY.value = self.innerHeight
+})
 </script>
