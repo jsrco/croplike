a6c825e261a9b98cb2eca26fad1de0c708c8f5f2
Ryan Orlando <rorlando@evidentchange.org>
2024-01-19 17:28:39 -0800

    04

--- src/scripts/godot/map/map.gd
+++ src/scripts/godot/map/map.gd
@@ -1,14 +1,21 @@
 class_name Map
 extends Node2D
 
+@export var fov_radius: int = 8
+
 var map_data: MapData
 
 @onready var dungeon_generator: DungeonGenerator = $"Dungeon Generator"
+@onready var field_of_view: FieldOfView = $"Field Of View"
 
 
 func generate(player: Entity) -> void:
 	map_data = dungeon_generator.generate_dungeon(player)
 	_place_tiles()
+
+
+func update_fov(player_position: Vector2i) -> void:
+	field_of_view.update_fov(map_data, player_position, fov_radius)
 
 
 func _place_tiles() -> void:
